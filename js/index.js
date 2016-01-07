cd72e35e48b6b254dfea0116ab80333211e4c19e
BillChabzz <chaby9.8@gmail.com>
2016-01-07 13:03:41 +0300

    used a library to play background video reduce file size

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$('document').ready(function() {
+	var options = { videoId: 'OiTiKOy59o4', start: 2 };
+	$('#wrapper').tubular(options);
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+});
